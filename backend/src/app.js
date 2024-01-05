8484f81564d4b81e5af586981318f774abced960
romilgabani12 <romilgabani0870@gmail.com>
2024-01-05 13:34:28 +0530

    All setup configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,30 @@
 import express from "express";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
 
-export {app}
+// frontend -- get data in -- JSON 
+app.use(express.json({limit : "16kb"}))
+
+// frontend -- get data in URL
+app.use(express.urlencoded({extended: true , limit : "16kb"}))
+
+// store external file in static public folder -- like consider as own server
+app.use(express.static("public"))
+
+// to set cokkieParser()
+app.use(cookieParser())
+
+
+
+
+
+
+export { app }
