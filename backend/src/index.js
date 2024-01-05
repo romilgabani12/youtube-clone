19593b873d0ca4bf27fcd5d9429ea749067981ad
romilgabani12 <romilgabani0870@gmail.com>
2024-01-05 11:49:52 +0530

    Connect to MongoDB atlas

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+
+
+
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+    
+})
+.catch((error) => {
+    console.log("MongoDB connection failed !!! ", error);
+})
