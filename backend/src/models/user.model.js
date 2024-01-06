8647e691b3f8522641ed9e337fa0712f25054fc1
romilgabani12 <romilgabani0870@gmail.com>
2024-01-06 17:11:04 +0530

    generate access token and refresh token using JWT

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -58,6 +60,70 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+
+
+/***********************************************************************************************************************
+It looks like you're using Mongoose middleware in Node.js to hash a user's password before saving it to the database. 
+This middleware is triggered before the "save" event of a Mongoose model.
+
+The pre method is used to define middleware that will be executed before a specified event, in this case, the "save" event.
+***************************************************************************************************************************/
+
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+
+/**************************************** 
+
+create a custom a methods
+
+*****************************************/
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+
+    return await bcrypt.compare(password, this.password)
+}
+
+
+userSchema.methods.generateAccessToken = function () {
+
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+
+userSchema.methods.generateRefreshToken = function () {
+
+    return jwt.sign(
+        {
+            _id: this._id,
+
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+
 
 
 export const User = mongoose.model("User", userSchema);
