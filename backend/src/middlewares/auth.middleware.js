ed4ca2911a2e35c0ef7fb5c1a6639b438308a278
romilgabani12 <romilgabani0870@gmail.com>
2024-01-07 18:43:41 +0530

    comment console.log statement

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -12,7 +12,7 @@ const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
 
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-        console.log(token);
+        // console.log(token);
 
         if (!token) {
             throw new ApiError(401, "Unauthorized request");
@@ -20,7 +20,7 @@ const verifyJWT = asyncHandler(async (req, _, next) => {
 
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        console.log(decodedToken);
+        // console.log(decodedToken);
 
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
 
