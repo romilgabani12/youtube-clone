fbe2319890e6a41b07c954e424546e593f9123d3
romilgabani12 <romilgabani0870@gmail.com>
2024-01-07 11:46:57 +0530

    solve refresh spelling mistake

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -50,7 +50,7 @@ const userSchema = new Schema(
             required: [true, "Password is required"]
         },
 
-        refereshToken: {
+        refreshToken: {
             type: String,
         }
 
