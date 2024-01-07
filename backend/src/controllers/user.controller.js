f72b699cb16d0a7624d7a2af6c26fb4fbc8e00be
romilgabani12 <romilgabani0870@gmail.com>
2024-01-07 13:58:59 +0530

    add logout functionality

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -175,8 +175,51 @@ const loginUser = asyncHandler(async (req, res) => {
 
 
 
+/***********************
+ logout user
+************************/
+
+const logoutUser = asyncHandler(async (req, res) => {
+
+
+    // to remove refreshToken in database
+
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+
+    // to remove refreshToken in cookies
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(
+            new ApiResponse(200, {}, "User logged out successfully")
+        )
+
+
+
+})
+
+
 
 export {
     registerUser,
     loginUser,
+    logoutUser,
 }
