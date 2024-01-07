c4f2425cc49a4c035fa3b2541455a7381b04db8d
romilgabani12 <romilgabani0870@gmail.com>
2024-01-07 14:00:20 +0530

    add logout route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 
@@ -23,7 +24,12 @@ router.route("/register").post(
     registerUser
 )
 
+
 router.route("/login").post(loginUser);
+
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 
 export default router;
