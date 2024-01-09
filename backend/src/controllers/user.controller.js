c5c9db74894ed6a38054defd27a3dc6452646132
romilgabani12 <romilgabani0870@gmail.com>
2024-01-09 10:50:41 +0530

    remove import User model because already exists

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,7 +4,6 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.model.js";
 import mongoose from "mongoose";
 
 /*************************************************
